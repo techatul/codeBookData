cd265c3db7cc6ef0aeada8123ca91aea83d40048
techatul <30597534+techatul@users.noreply.github.com>
2025-03-20 00:25:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
-server.use(middlewares);
-server.use(router);
+const server = jsonServer.create()
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('/api', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
